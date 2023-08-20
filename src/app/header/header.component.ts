8a380b544f75ce7c7db947165109eefc172dc6ca
ciedar <dciesla545@gmail.com>
2023-08-20 22:41:44 +0200

    headerComponent

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  num: number;
+  @ViewChild('hamburger', { static: false }) hmburgerButton: ElementRef<HTMLElement>
+
+
+  dropdownToggle() {
+    this.hmburgerButton.nativeElement.classList.toggle('show')
+  }
+
+  ngOnInit(): void {
+
+  }
+}
