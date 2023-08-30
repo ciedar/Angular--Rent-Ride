bfcdd39cdbf6a45549afbd99673283084bf84152
ciedar <dciesla545@gmail.com>
2023-08-30 19:15:07 +0200

    postItemByUserLogic

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -7,24 +7,14 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class DatabaseService implements OnInit {
-  user: User
-  firebaseUserItemsLink: string = `https://tablica-20451-default-rtdb.europe-west1.firebasedatabase.app/items.json`
+export class DatabaseService {
+  postItemByUserFirebaseLink: string = `https://tablica-20451-default-rtdb.europe-west1.firebasedatabase.app/items.json`
   constructor(private firebase: FirebaseService, private httpClient: HttpClient) {
 
   }
 
-  ngOnInit(): void {
-    this.firebase.user.subscribe((data: User) => {
-      this.user = data;
-    })
-  }
-
-  additem() {
-    return this.httpClient.put<User>(this.firebaseUserItemsLink, {
-      name: 'dupek',
-      id: 12
-    });
+  additem(user: User) {
+    return this.httpClient.put<User>(this.postItemByUserFirebaseLink, user);
   }
 
 }
--- src/app/user-cockpit/add-post/add-post.component.ts
+++ src/app/user-cockpit/add-post/add-post.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 import { DatabaseService } from 'src/app/services/database.service';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
   styleUrls: ['./add-post.component.css']
 })
-export class AddPostComponent {
+export class AddPostComponent implements OnInit {
+  user: User;
 
-
-  constructor(private databaseService: DatabaseService) {
+  constructor(private databaseService: DatabaseService, private firebase: FirebaseService) {
 
   }
+  ngOnInit(): void {
+    this.firebase.user.subscribe((data: User) => {
+      this.user = data;
+    })
+  }
+
   postItem() {
-    this.databaseService.additem().subscribe();
+    console.log(this.user);
+    this.databaseService.additem(this.user).subscribe();
   }
 }
