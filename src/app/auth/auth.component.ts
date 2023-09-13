d9d6f77ad99cdd85d52343611bbe827fd8af7d21
ciedar <dciesla545@gmail.com>
2023-09-13 21:21:05 +0200

    iamDumbWasTryingPutAsyncValidatorIntoSyncPlaceValidatorXDDDDD

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, ValidationErrors, Form, AbstractControl } from '@angular/forms'
 import { AuthResponseData, FirebaseService } from '../services/firebase.service';
-import { Observable, debounceTime, map, mergeMap, of, switchMap, take, tap } from 'rxjs';
+import { Observable, catchError, debounceTime, map, mergeMap, of, switchMap, take, tap, throwError } from 'rxjs';
 import { User } from '../models/user.model';
 import { Router } from '@angular/router';
 import { DatabaseService } from '../services/database.service';
@@ -19,6 +19,7 @@ export class AuthComponent implements OnInit {
   registerForm: FormGroup
   loginMode: boolean = false;
   error: null
+  disabled: boolean = false
   user: User
   usernames: string[] = [];
   constructor(private firebase: FirebaseService, private router: Router, private database: DatabaseService) {
@@ -34,12 +35,13 @@ export class AuthComponent implements OnInit {
     })
 
     if (!this.loginMode) {
-      this.registerForm.addControl('username', new FormControl(null, [this.customValidator.bind(this)]));
+      this.registerForm.addControl('username', new FormControl(null, Validators.required, this.validateUsername.bind(this)));
     }
   }
 
 
   submitForm(data: FormGroup) {
+
     this.firebase.user.subscribe((data: User) => {
       this.user = data;
 
@@ -67,123 +69,19 @@ export class AuthComponent implements OnInit {
     this.loginMode = !this.loginMode;
   }
 
-  // checkUsernameValidator(data: FormControl): Observable<ValidationErrors | null> {
-  //   return this.database.getUserList().pipe(
-  //     map(resData => {
-  //       console.log(resData)
-  //       if (resData.includes(data.value)) {
-  //         console.log('dupa')
-  //         return { 'zajęty': true }
-  //       } else {
-  //         return null
-  //       }
-  //     })
-  //   )
-  // }
-  // checkUsernameValidator(data: FormControl): Observable<ValidationErrors | null> {
-  //   return this.database.getUserList().pipe(
-  //     map(resData => {
-  //       console.log(data.value)
-  //       console.log(resData);
-  //       if (resData.includes(data.value)) {
-  //         console.log(data.value)
-  //         console.log('Nazwa użytkownika jest zajęta');
-  //         this.registerForm.get('username').setErrors({ 'zajęty': true }); // Ustawienie błędu w polu username
-  //         return { 'zajęty': true };
-  //       } else {
-  //         this.registerForm.get('username').setErrors(null); // Usunięcie błędu w polu username
-  //         return null;
-  //       }
-  //     })
-  //   );
-  // }
 
-  // customValidator(): Promise<{ [username: string]: boolean }> {
-  //   let promise = new Promise((resolve, reject) => {
-  //     this.database.getUserList().subscribe(data => {
-  //       resolve(data)
-  //     })
-  //   })
-  //   const data = promise.then(data => { return data });
-  //   console.log(data)
-  //   return null
-  // }
-  // customValidator(control: FormControl): Observable<{ [username: string]: boolean }> | null {
-  //   this.database.getUserList().subscribe(resData => {
-  //     if (resData.includes(control.value)) {
-  //       console.log(resData);
-  //       console.log(control.value)
-  //       return { 'username': true };
-  //     } else {
-  //       return null
-  //     }
-  //   })
-  //   return null
-  // }
-  // customValidator(control: FormControl): Observable<{ [username: string]: boolean }> {
-  //   this.database.getUserList().pipe(
-  //     map(data => {
-  //       if (data.includes(control.value)) {
-  //         console.log(control.value)
-  //         return { 'nie': true }
-  //       } else {
-  //         return null
-  //       }
-  //     })
-  //   ).subscribe()
-  //   console.log('asdasd')
-  //   return null
-  // }
-  customValidator(control: FormControl): Observable<{ [username: string]: boolean } | null> {
+  validateUsername(data: FormControl): Observable<{ [key: string]: boolean }> {
     return this.database.getUserList().pipe(
-      map(data => {
-        if (data.includes(control.value)) {
-          console.log(control.value);
-          console.log(this.registerForm)
-          return { 'nie': true }; // Zwróć obiekt błędu, jeśli warunek jest spełniony
+      map(userList => {
+        if (userList.indexOf(data.value) !== -1) {
+          return { 'userExist': true }
         } else {
-          return null; // Zwróć null, jeśli warunek nie jest spełniony
+          return null;
         }
       })
-    );
+    )
   }
+
 
 }
 
-  // const usernameArr: string[] = [];
-  // console.log(data.value)
-  //  this.database.getUserList().pipe(
-    //   map(data => {
-      //     const values = Object.values(data);
-      //     for (let obj of values) {
-        //       usernameArr.push(obj.username);
-        //     }
-        //   })
-        // ).subscribe();
-        // return null;
-
-
-        // checkUsernameValidator(data: FormControl): Promise<ValidationErrors> {
-        //   return new Promise((resolve, reject) => {
-        //     this.database.getUserList().pipe(
-        //       map(data => {
-        //         const values = Object.values(data).map(b => b.username);
-        //         return values
-        //       })
-        //     ).subscribe((dataResponse) => {
-        //       console.log(dataResponse)
-        //       if (dataResponse.includes(data.value)) {
-        //         console.log('t', data.value)
-        //         console.log(this.registerForm);
-        //         resolve(
-        //           { 'zajęty': true }
-        //         )
-        //       } else {
-        //         console.log(this.registerForm);
-        //         console.log('n', data.value)
-        //         // reject(null);
-        //       }
-        //     });
-        //     return null
-        //   })
-        // }
